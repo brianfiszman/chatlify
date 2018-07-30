32eb7a398917f6de32e46394e3ae880a0db7273d
Brian Fiszman <brianezequielfiszman@gmail.com>
2018-07-30 20:55:56 -0300

    Unstructurd state into user and fireRedirect.

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -4,57 +4,54 @@ import InputUser from "./InputUser";
 import Header from "./Header";
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      user: "",
-      fireRedirect: false
-    };
+		this.state = {
+			user: "",
+			fireRedirect: false
+		};
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.updateUser = this.updateUser.bind(this);
-  }
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.updateUser = this.updateUser.bind(this);
+	}
 
-  handleSubmit(e) {
-    e.preventDefault();
-    if (this.state.user) {
-      this.setState({
-        fireRedirect: true
-      });
-    } else {
-      alert("Por favor introducir usuario");
-    }
-  }
+	handleSubmit(e) {
+		const { user } = this.state;
 
-  updateUser(e) {
-    this.setState({
-      user: e.target.value
-    });
-  }
+		e.preventDefault();
+		if (user) {
+			this.setState({
+				fireRedirect: true
+			});
+		} else {
+			alert("Por favor introducir usuario");
+		}
+	}
 
-  render() {
-    return (
-      <article className="row align-content-center h-100">
-        <section className="col h-50">
-          <section className="row align-items-center align-content-center justify-content-center h-100">
-            <section className="col">
-              {this.state.fireRedirect ? (
-                <Redirect push to={"/chatapp/" + this.state.user} />
-              ) : (
-                ""
-              )}
-              <Header />
-              <InputUser
-                handleSubmit={this.handleSubmit}
-                updateUser={this.updateUser}
-              />
-            </section>
-          </section>
+	updateUser(e) {
+		this.setState({
+			user: e.target.value
+		});
+	}
+
+	render() {
+		const { fireRedirect, user } = this.state;
+
+		return (
+  <article className="row align-content-center h-100">
+    <section className="col h-50">
+      <section className="row align-items-center align-content-center justify-content-center h-100">
+        <section className="col">
+          {fireRedirect && <Redirect push to={"/chatapp/" + user} />}
+          <Header />
+          <InputUser handleSubmit={this.handleSubmit} updateUser={this.updateUser} />
         </section>
-      </article>
-    );
-  }
+      </section>
+    </section>
+  </article>
+		);
+	}
 }
 
 export default Login;
