b792e229d527e9d3dcf49f30ef79000258ed3540
Brian Fiszman <brian.fiszman@codeait.com>
2018-07-31 09:46:38 -0300

    Set base structure for redux app

---
+++ src/Common/reducer.js
@@ -0,0 +1,14 @@
+import { SET_USER } from "./actions";
+
+const initialState = { user: [] };
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./Common/store";
 import App from "./App";
-import "./Resources/SASS/main.scss"
-// import registerServiceWorker from "./registerServiceWorker";
+import "./Resources/SASS/main.scss";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-// registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
