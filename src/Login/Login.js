5de83f933bc588e70f9287e2d4f9698e2fc55267
Brian Fiszman <brianezequielfiszman@gmail.com>
2018-07-31 23:19:28 -0300

    The greeting header is now stateless

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import InputUser from "./InputUser";
-import Header from "./Header";
+
+const Header = () => (
+  <h1 className="col-12 col-md-8 col-lg-6 mx-auto pb-5">Bienvenido usuario</h1>
+);
 
 class Login extends Component {
   constructor(props) {
