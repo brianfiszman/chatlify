5106e76442dd39e89a3177b2c92b92a910610c9f
Brian Fiszman <brianezequielfiszman@gmail.com>
2018-08-22 23:38:58 -0300

    Extracted logic, refactored and transpiled into ES6 chat.js

--- src/server/app/common/procedures.js
+++ src/server/app/common/procedures.js
@@ -25,3 +25,27 @@ export const queryLastResult = (db, collection) => {
       return err;
     });
 };
+
+export const queryOneResult = (db, res, collection, details) => {
+  db.collection(collection).findOne(details, (err, item) => {
+    if (err) {
+      res.send(err);
+      res.end();
+    } else {
+      res.send(item);
+      res.end();
+    }
+  });
+};
+
+export const insertToCollection = (db, res, collection, message) => {
+  return db.collection(collection).insert(message, (err, result) => {
+    if (err) {
+      res.send(err);
+      res.end();
+    } else {
+      res.send(result.ops[0]);
+      res.end();
+    }
+  });
+};
--- src/server/app/controllers/chat.js
+++ src/server/app/controllers/chat.js
@@ -1,21 +1,27 @@
-import { queryAllResult, queryLastResult } from "../common/procedures";
+import {
+  queryAllResult,
+  queryLastResult,
+  insertToCollection,
+  queryOneResult
+} from "../common/procedures";
 
 const ObjectID = require("mongodb").ObjectID;
 
 const collection = "messages";
 
 const getLastResult = async db => {
-  let result = await queryLastResult(db, collection);
+  const result = await queryLastResult(db, collection);
   return result;
 };
 
 const getAllResult = async db => {
-  let result = await queryAllResult(db, collection);
+  const result = await queryAllResult(db, collection);
   return result;
 };
 
 const sendAllResult = async (db, res) => {
-  let allResults = await getAllResult(db, collection);
+  const allResults = await getAllResult(db, collection);
+
   res.send(allResults);
   res.end();
 };
@@ -23,12 +29,15 @@ const sendAllResult = async (db, res) => {
 const poll = async (id, res, db) => {
   if (id) {
     var i = 0;
+
     let timeout = async () => {
       let lastResult = await getLastResult(db);
+
       if (lastResult && lastResult != id) {
         i = 100;
         await sendAllResult(db, res);
       }
+
       if (++i < 100) {
         setTimeout(timeout, 1000);
       }
@@ -49,15 +58,8 @@ exports.create = (req, res, db) => {
       text: req.body.text,
       timestamps: new Date().toUTCString()
     };
-    db.collection(collection).insert(message, (err, result) => {
-      if (err) {
-        res.send(err);
-        res.end();
-      } else {
-        res.send(result.ops[0]);
-        res.end();
-      }
-    });
+
+    insertToCollection(db, res, collection, message);
   }
 };
 
@@ -70,13 +72,6 @@ exports.findOne = (req, res, db) => {
   const details = {
     _id: new ObjectID(id)
   };
-  db.collection(collection).findOne(details, (err, item) => {
-    if (err) {
-      res.send(err);
-      res.end();
-    } else {
-      res.send(item);
-      res.end();
-    }
-  });
+
+  queryOneResult(db, res, collection, details);
 };
