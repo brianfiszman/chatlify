37d980a44ff2df6f390f41224213de461bbb7327
Brian Fiszman <brianezequielfiszman@gmail.com>
2018-08-23 18:33:59 -0300

    Merge branch 'develop'

--- src/server/app/common/procedures.js
+++ src/server/app/common/procedures.js
@@ -1,51 +1,29 @@
-export const queryAllResult = (db, collection) => {
-  return db
+export const queryAllResult = (db, collection) =>
+  db
     .collection(collection)
     .find()
     .toArray()
-    .then(result => {
-      return result;
-    })
-    .catch(err => {
-      return err;
-    });
-};
+    .then(result => result)
+    .catch(err => err);
 
-export const queryLastResult = (db, collection) => {
-  return db
+export const queryLastResult = (db, collection) =>
+  db
     .collection(collection)
     .find()
     .sort({ _id: -1 })
     .limit(1)
     .toArray()
-    .then(result => {
-      return result[0]._id;
-    })
-    .catch(err => {
-      return err;
-    });
-};
+    .then(result => result[0]._id)
+    .catch(err => err);
 
-export const queryOneResult = (db, res, collection, details) => {
+export const queryOneResult = (db, res, collection, details) =>
   db.collection(collection).findOne(details, (err, item) => {
-    if (err) {
-      res.send(err);
-      res.end();
-    } else {
-      res.send(item);
-      res.end();
-    }
+    res.send(err || item);
+    res.end();
   });
-};
 
-export const insertToCollection = (db, res, collection, message) => {
-  return db.collection(collection).insert(message, (err, result) => {
-    if (err) {
-      res.send(err);
-      res.end();
-    } else {
-      res.send(result.ops[0]);
-      res.end();
-    }
+export const insertToCollection = (db, res, collection, message) =>
+  db.collection(collection).insertOne(message, (err, result) => {
+    res.send(err || result.ops[0]);
+    res.end();
   });
-};
