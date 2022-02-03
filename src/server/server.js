4ae3398fd133d5ac0818ad5625099024a26104f6
Brian Fiszman <bfiszman_ext@censys.io>
2022-02-03 09:36:22 -0300

    Change server code to new mongo client

--- src/server/server.js
+++ src/server/server.js
@@ -12,22 +12,20 @@ app.set("port", process.env.PORT || 5000);
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(
-  db.url,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    if (err) return console.log(err);
-    require(__dirname + "/app/routes")(app, io, client.db("chatlify"));
+const client = new MongoClient(db.url, { useNewUrlParser: true, useUnifiedTopology: true });
+  
+client.connect((err) => {
+  if (err) return console.log(err);
+  require(__dirname + "/app/routes")(app, io, client.db("chatlify"));
 
-    const server = app.listen(app.get("port"), () => {
-      console.log("We are live on ", app.get("port"));
-    });
+  const server = app.listen(app.get("port"), () => {
+    console.log("We are live on ", app.get("port"));
+  });
 
-    io.attach(server);
+  io.attach(server);
 
-    io.on("connection", () => {
-      console.log("HELLO");
-    });
-  },
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  io.on("connection", () => {
+    console.log("HELLO");
+  });
+}
 );
